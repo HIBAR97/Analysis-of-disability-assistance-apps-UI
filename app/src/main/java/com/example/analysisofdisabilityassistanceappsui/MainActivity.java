0a5b048168269ebf330c2080b2142e5c7a826417
HIBER97 <hibar9710@gmail.com>
2022-01-04 21:50:57 +0900

    Update 22.01.04 21:50 ------------------------------- Add: Add App "Voice eye" (Main page, Fragment page) Add Java file all xml
    -------------------------------
    Fix :
    
    -------------------------------
    Error (Have to Fix) :
    -Overall-
    App is not working.
    
    -MainActivity-
    Null
    
    -Be_My_Eyes_Mainscreen-
    NUll
    
    -VoiceEye Main-
    Null
    
    -VoiceEye Fragment-
    Null
    
    -------------------------------

--- app/src/main/java/com/example/analysisofdisabilityassistanceappsui/MainActivity.java
+++ app/src/main/java/com/example/analysisofdisabilityassistanceappsui/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.analysisofdisabilityassistanceappsui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnBME;
+        Button btnVE;
+
+        btnBME = (Button)findViewById(R.id.btnBME);
+        btnVE = (Button)findViewById(R.id.btnVE);
+
+
+        btnBME.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //takePic으로 이동
+                Intent intent = new Intent (getApplicationContext(), be_my_eyes_mainscreen.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //takePic으로 이동
+                Intent intent = new Intent (getApplicationContext(), voiceeyemain.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/analysisofdisabilityassistanceappsui/be_my_eyes_mainscreen.java
@@ -0,0 +1,14 @@
+package com.example.analysisofdisabilityassistanceappsui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class be_my_eyes_mainscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.be_my_eyes_mainscreen);
+    }
+}
